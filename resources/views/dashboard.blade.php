a70bc433c70888f53c222e0f2242dc73c9cf5e4e
emredipi <mail@emredipi.com>
2021-06-04 00:58:26 +0300

    bold titles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        {{ __('Dashboard') }}
     </x-slot>
 
     <div class="py-12">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,9 @@
 <!-- Page Heading -->
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $header }}
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ $header }}
+            </h2>
         </div>
     </header>
     <!-- Page Content -->
