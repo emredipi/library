f3c478704e5ab844d1f964ec171d5396c2c6e92e
emredipi <mail@emredipi.com>
2021-06-06 01:55:59 +0300

    button and icons

--- resources/views/components/delete-button.blade.php
+++ resources/views/components/delete-button.blade.php
@@ -1,6 +1,12 @@
-@props(['action-url'])
+@props(['action-url','icon' => false])
 @php $id = Str::random(10); @endphp
-<button type="submit" class="text-red-500" form="{{$id}}">Sil</button>
+@if($icon)
+    <x-button form="{{$id}}" class="bg-red-600 hover:bg-red-500">
+        <x-icon.delete/>
+    </x-button>
+@else
+    <button type="submit" class="text-red-500" form="{{$id}}">Sil</button>
+@endif
 @push('scripts')
     <form
         action="{{$actionUrl}}"
